7a3c1b22a54360c0e96a9ea2e20e71d7ad6abe68
clay_laptop <tonofclay3@gmail.com>
2022-10-23 20:41:58 -0700

    starting lab 4

---
+++ Lab4_numpy_and_linear_algebra/svd_compress.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt    # need for plotting an image
+import matplotlib.image as mpimg   # need for image loading and saving
+import numpy as np                 # need for manipulating arrays
+from numpy import linalg as la     # need for finding SVD
+import os
+print(os.chdir('C:/Users/tonof/OneDrive/Documents/Code/CSE30/Lab4_numpy_and_linear_algebra'))
+print(os.getcwd())
+# mpimg.imread('octopus.jpg')        # you need to download the image from Canvas/Files/Labs
+# plt.imshow(img)
+# plt.show()
+# print(img.ndim)
+# X, Y, Z = img.shape                # get the image array dimensions
+# print(X, Y, Z)
+# print(img.dtype)                   # array value type
+# print(img.max())                   # max number in the array
+# print(img.min())                   # min number in the array
