a6f03f97a1cda28d822a91b7b5d01e37b6c9d50c
clay_pc <tonofclay3@gmail.com>
2022-10-25 17:05:32 -0700

    finished image compression!

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 import numpy as np
-arr = np.array([1, 2, 3, 4, 5])
 
+# eigenvectors and eigenvalues
+A = np.array([[1, -1], [2, 4]])
+# print(A)
+evalues, evectors = np.linalg.eig(A)
+# print(evectors)
 
-arr_to2D = arr.reshape(5,1)
-print(arr_to2D)
-print(arr_to2D.reshape(-1))
+evectors = np.transpose(evectors) # we need to transpose the matrix to get the eigenvectors
+for i in range(len(evalues)):
+    print('A x:', np.dot(A, evectors[ i]))
+    print('lambda x:', np.dot(evalues[ i],evectors[ i]))
