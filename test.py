a57561fb05293f4f81e9085397c3abf39f5318ee
clay_pc <tonofclay3@gmail.com>
2022-11-02 14:34:22 -0700

    Starting cryptography

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
-from matplotlib import pyplot as plt
 import numpy as np
-import math
-x = np.arange(0, math.pi*2, 0.05) # make an array of values from 0.0 to 2π with a step 0.05
-y = np.sin(x)                     # make an array of values using the function sin(x)
-plt.plot(x,y)                     # create a line plot
-plt.xlabel("angle")               # add a label for the x axis
-plt.ylabel("sine")                # add a label for the y axis
-plt.title('sine wave')            # add a title for the plot
-print(y)
-# plt.show()                        # create a GUI window and visualize the plot
+
+
+
+output = ''
+text = 'hello'
+output = output.join([format(ord(i), "08b") for i in text])
+# print(output)
+
+
+# print(format(ord('A') + 3))
+
+byte = format(ord('z'), "08b")
+
+print(byte)
+
+print(chr(int(byte,2)))
+
