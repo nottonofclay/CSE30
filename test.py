8d72dd07375c15214403ba18cc8dd52a15b31e83
clay_pc <tonofclay3@gmail.com>
2022-11-03 17:09:12 -0700

    testing

--- test.py
+++ test.py
@@ -4,4 +4,9 @@ os.chdir('C:/Users/tonof/OneDrive/Documents/Code/CSE30/PA3_Cryptography')
 
 image = cv2.imread('redbox.jpg')
 
-print(image)
+print(type(image))
+
+for i in range(len(image)):
+    for j in range(len(image[i])):
+        print(f'{i}.{j}: {image[i][j]}')
+
